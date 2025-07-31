74a92621b64b79ac618fdc6279ee47dded1c6940
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-07-31 20:51:20 +0700

    Improve login

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { pool } from 'src/db/database';
@@ -11,11 +15,13 @@ export class AuthService {
   async register(username: string, password: string) {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
+
     try {
       await pool.query(
         'INSERT INTO users (username, password) VALUES ($1, $2)',
         [username, hashedPassword],
       );
+      
       return { message: 'User registered successfully' };
     } catch (error) {
       console.error('Registration error:', error);
@@ -26,19 +32,27 @@ export class AuthService {
   // LOGIN
   async login(username: string, password: string) {
     try {
-      const user = await pool.query('SELECT * FROM users WHERE username = $1', [
+      const data = await pool.query('SELECT * FROM users WHERE username = $1', [
         username,
       ]);
-      const user_password = user.rows[0].password;
 
-      if (password !== user_password) {
-        return { message: 'Invalid Password' };
+      const user = data.rows[0];
+
+      if (!user || user.length === 0) {
+        throw new NotFoundException(`${username} not found`);
+      }
+
+      const isValidPassword = await bcrypt.compare(password, user.password);
+
+      if (!isValidPassword) {
+        throw new UnauthorizedException('Invalid password');
       }
 
       const token = this.jwtService.sign(
         { username: user.rows[0].username },
         { expiresIn: '1d' },
       );
+
       return { user: user.rows[0], token };
     } catch (error) {
       console.log('Login error: ', error);
