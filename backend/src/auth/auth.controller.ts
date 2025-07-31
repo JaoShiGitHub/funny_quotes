306ea744972bfb9acdd71f2b31447dc198f74427
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-07-31 20:43:18 +0700

    Creat register controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,29 +1,50 @@
-import { Body, Controller, Post, Res } from "@nestjs/common";
-import { AuthService } from "./auth.service";
+import {
+  Body,
+  Controller,
+  BadRequestException,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
 import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-    @Post('register')
-    register() {
-        return 'Hello Register';
+// POST /auth/register
+  @Post('register')
+  async register(
+    @Body() body: { username: string; password: string },
+    @Res() res: Response,
+  ) {
+    const { username, password } = body;
+    try {
+      const response = await this.authService.register(username, password);
+        console.log("Register checking: ", response);
+      res.status(201).json(response);
+    } catch (error) {
+      console.error('Registration error:', error);
+      throw new BadRequestException('Could not register user');
     }
+  }
 
-    @Post('login')
-    async login(@Body() body: {username: string; password: string}, @Res() res: Response) {
-        const {username, password} = body;
-        const user = await this.authService.login(username, password);
+// POST /auth/login
+  @Post('login')
+  async login(
+    @Body() body: { username: string; password: string },
+    @Res() res: Response,
+  ) {
+    const { username, password } = body;
+    const user = await this.authService.login(username, password);
 
-        res.cookie('token', user.token, {
-         httpOnly: true,
-         maxAge: 24 * 60 * 60 * 1000,
-         sameSite: 'strict',
-         secure: false,
-        });
+    res.cookie('token', user.token, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+      sameSite: 'strict',
+      secure: false,
+    });
 
-        return res.json({ user: user.user });
-    }
-   
+    return res.json({ user: user.user });
+  }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 import { pool } from 'src/db/database';
 
 @Injectable({})
 export class AuthService {
   constructor(private jwtService: JwtService) {}
 
+  // REGISTER
+  async register(username: string, password: string) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    try {
+      await pool.query(
+        'INSERT INTO users (username, password) VALUES ($1, $2)',
+        [username, hashedPassword],
+      );
+      return { message: 'User registered successfully' };
+    } catch (error) {
+      console.error('Registration error:', error);
+      throw new Error('Username may already exist or something went wrong');
+    }
+  }
+
+  // LOGIN
   async login(username: string, password: string) {
     try {
       const user = await pool.query('SELECT * FROM users WHERE username = $1', [
