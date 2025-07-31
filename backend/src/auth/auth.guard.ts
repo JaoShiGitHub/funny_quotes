c14e794f3557fe72dc5e6b6fd50d4487fe6b03f4
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-07-31 21:49:43 +0700

    Create auth guard

---
+++ backend/src/auth/auth.guard.ts
@@ -0,0 +1,30 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = request.cookies?.token;
+
+    if (!token) {
+      throw new UnauthorizedException('Please login first.');
+    }
+
+    try {
+      const decoded = this.jwtService.verify(token);
+      request['customer'] = decoded;
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException(`Invalid token: ${error.message}`);
+    }
+  }
+}
