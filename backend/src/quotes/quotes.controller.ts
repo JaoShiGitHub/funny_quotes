48023bdf549f1b47498a71570202d156641fe925
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-08-01 19:33:24 +0700

    Implement voting

--- backend/src/quotes/quotes.controller.ts
+++ backend/src/quotes/quotes.controller.ts
@@ -1,20 +1,32 @@
-import { UseGuards, Controller, Get } from '@nestjs/common';
+import { UseGuards, Controller, Get, Post, Req, Body, Res } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { QuotesService } from './quotes.service';
+import type { Request, Response } from 'express';
 
 @UseGuards(AuthGuard)
 @Controller('quotes')
 export class QuotesController {
-  constructor(private quotesService: QuotesService) {}
+  constructor(private quotesService: QuotesService) { }
 
   @Get('all_quotes')
   async getQuotes() {
-    //   console.log('🔥 QuotesController is working');
     const quotes_data = await this.quotesService.getQuotes();
 
     return {
       message: 'All quotes fetched successfully',
       quotes_data,
     };
+  }
+
+  @Post('vote')
+  async voteQuote(@Req() req: Request, @Res() res: Response, @Body() body) {
+    const userId = req['currentUser']?.id;
+    const quoteId = body.quote_id
+    try {
+      await this.quotesService.voteQuote(userId, quoteId);
+      return res.status(201).json({ message: "Voting completed" })
+    } catch (error) {
+      return res.status(400).json({message: "Voting failed"})
+    }
   }
 }
--- backend/src/quotes/quotes.service.ts
+++ backend/src/quotes/quotes.service.ts
@@ -7,19 +7,35 @@ export class QuotesService {
     // console.log('QuotesService.getQuotes() was called ✅');
 
     const response = await pool.query(`
-      SELECT 
-        q.quote_id, 
-        q.quote, 
-        q.votes,
-        u.user_id AS author_id,
-        u.username AS author
-      FROM quotes q
-      JOIN users u ON q.author_id = u.user_id
-    `);
+  SELECT 
+    q.quote_id,
+    q.quote,
+    u.user_id AS author_id,
+    u.username AS author,
+    q.votes AS vote,
+    COALESCE(ARRAY_AGG(qv.user_id), '{}') AS voters
+  FROM quotes q
+  JOIN users u ON q.author_id = u.user_id
+  LEFT JOIN quote_votes qv ON q.quote_id = qv.quote_id
+  GROUP BY q.quote_id, q.quote, q.votes, u.user_id, u.username
+`);
 
     if (response.rows.length === 0) {
       throw new NotFoundException('Quotes not found');
     }
     return response.rows;
+  }
+
+  async voteQuote(userId: number, quoteId: number) {
+    try {
+      await pool.query(
+        'INSERT INTO quote_votes (quote_id, user_id) VALUES ($1, $2)',
+        [quoteId, userId],
+      );
+      return { message: 'Vote Successfully' };
+    } catch (error) {
+      console.error('Voting failed:', error);
+      throw new Error('Voting failed');
+    }
   }
 }
